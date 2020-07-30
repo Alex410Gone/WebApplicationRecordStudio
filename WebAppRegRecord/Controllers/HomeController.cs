73f89bf45ed4e659265a76701ef432c0700f855f
alex410gone@gmail.com <alex410gone@gmail.com>
2020-07-30 12:47:00 +0300

    Добавьте файлы проекта.

---
+++ WebAppRegRecord/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppRegRecord.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppRegRecord/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppRegRecord.MvcApplication" Language="C#" %>
